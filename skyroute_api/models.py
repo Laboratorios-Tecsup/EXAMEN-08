803866c1bfc24736d809ceb831a6575200e0dfce
Andy-CE17 <andy.campos@tecsup.edu.pe>
2026-05-08 19:00:28 -0500

    agrego los modelos Flight y Airline

--- skyroute_api/models.py
+++ skyroute_api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Airline(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    country = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Airline'
+        verbose_name_plural = 'Airlines'
+
+    def __str__(self):
+        return self.name
+
+
+class Flight(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    airline = models.ForeignKey(
+        Airline,
+        on_delete=models.CASCADE,
+        related_name='flights'
+    )
+
+    class Meta:
+        ordering = ['code']
+        verbose_name = 'Flight'
+        verbose_name_plural = 'Flights'
+
+    def __str__(self):
+        return self.code
